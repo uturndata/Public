86deac6af083fdfad9726d2b3559760b77d03967
Guy Denney <gdenney@ututndata.com>
2015-04-16 09:43:26 -0500

    no message

--- config.py
+++ config.py
@@ -6,8 +6,10 @@ config = {
     'aws_secret_key': '',
 
     # EC2 info about your server's region
-    'ec2_region_name': 'us-east-1',
-    'ec2_region_endpoint': 'ec2.us-east-1.amazonaws.com',
+    #'ec2_region_name': 'us-east-1',
+    #'ec2_region_endpoint': 'ec2.us-east-1.amazonaws.com',
+    'ec2_region_name': 'RegonName',
+    'ec2_region_endpoint': 'RegonEndpoint',
 
     # Tag of the EBS volume you want to take the snapshots of
     #'tag_name': 'tag:MakeSnapshot',
@@ -16,9 +18,13 @@ config = {
 
     # Number of snapshots to keep (the older ones are going to be deleted,
     # since they cost money).
-    'keep_day': DayNumber,
-    'keep_week': WeekNumber,
-    'keep_month': MonthNumber,
+    #'keep_day': 7,
+    #'keep_week': 5,
+    #'keep_month': 13,
+
+    'keep_day': DayCount,
+    'keep_week': WeekCount,
+    'keep_month': MonthCount,
 
     # Path to the log for this script
     'log_file': '/tmp/makesnapshots.log',
